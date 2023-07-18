8be82e0c266e3474ed6558969521444acbf9ea09
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-07-17 20:00:39 -0600

    Update about content, update header css - add positive: relative to me image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./about.css"
 // import {FiAward} from "react-icons/fi"
 // import {FiUsers} from "react-icons/fi"
 // import {AiFillFolderOpen} from "react-icons/ai"
 
 const About = () => {
+  const [readMore, setReadMore] = useState(false)
+
+  function handleClick() {
+    setReadMore(readMore => !readMore)
+  }
+
   return (
     <section id="about">
       <h5>Get To Know</h5>
@@ -30,18 +36,33 @@ const About = () => {
               <small>Building Portfolio</small>
             </article>
           </div> */}
-          <p>I am a software engineer trained through the Flatiron School. I am well-versed in JavaScript, Python3, SQL, and CSS. The frameworks I have experience working with include React.js, Next.js, Flask, PostgreSQL, SQLite3, SQLAlchemy, Sass, and Semantic-UI. I'm always excited for a new challenge and the opportunity to learn new languages and frameworks.
+        <p>I am a software engineer fluent in JavaScript, Python, SQL, HTML, and CSS. 
+        I'm experienced with React, Flask, and SQLAlchemy frameworks as well as Tailwind and Sass for custom-CSS.
+        </p>
+        <p>I became interested in web development after completing a tutorial course and seeing how many languages and frameworks there were to master. 
+        As a former educator, I am passionate about learning and describe myself as a lifelong learner.
+        </p>
+        <div className="see-more">
+        <button className="see-more-btn" onClick={handleClick}>{readMore? "Read less..." : "Read more..."}</button>
+        </div>
+        {readMore ?
+        <div>
+          <p>I value working in a dynamic industry where continually developing skill sets is essential and where there is always something to learn. 
+          I spent the past eight years working in public education as a high school band teacher. Through this experience, 
+          I developed strong communication skills, learned to collaborate and problem-solve with a variety of stakeholders, 
+          and constantly modeled leadership. I am incredibly detail-oriented thanks to years of honing my craft as a flute player 
+          and helping students refine their musical skills. Being an educator also taught me to balance my detail-oriented nature with time 
+          constraints and to be extremely efficient.</p>
+          <p>I would love to apply my passion for learning, soft skills developed through teaching, 
+          and software engineering skill set to build innovative and enriching web applications and user experiences.
           </p>
-          <p>I became interested in web development after completing a tutorial course and seeing how many languages and frameworks there were to master. As a former educator, I am passionate about learning and describe myself as a lifelong learner. I value working in a dynamic industry where continually developing skill sets is essential and where there is always something to learn.
-I spent the past eight years working in public education as a high school band teacher. Through this experience, I developed strong communication skills, learned to collaborate and problem-solve with a variety of stakeholders, and constantly modeled leadership. 
-I am incredibly detail-oriented thanks to years of honing my craft as a flute player and helping students refine their musical skills. Being an educator also taught me to balance my detail-oriented nature with time constraints and to be extremely efficient.
+          <p>When I'm not coding, I love to spend time with my husband Steve and our two dogs, Molly and Olivia. 
+          We built a home in Denver and are passionate about renovating, gardening, and home brewing beer! 
+          I also love to travel and am looking forward to taking my first international trip - to England! - this winter.
           </p>
-          <p>I would love to apply my passion for learning, soft skills developed through teaching, and software engineering skill set to build innovative and enriching web applications and user experiences.
-          </p>
-          <p>When I'm not coding, I love to spend time with my husband Steve and our two dogs, Molly and Olivia. We built a home in Denver and are passionate about renovating, gardening, and home brewing beer! I also love to travel and am looking forward to taking my first international trip - to England! - this winter.
-          </p>
-
-          <a href="#contact" className="btn btn-primary">Let's Connect</a>
+          </div> : null}
+        
+        <a href="#contact" className="btn btn-primary">Let's Connect</a>
         </div>
       </div>
     </section>
