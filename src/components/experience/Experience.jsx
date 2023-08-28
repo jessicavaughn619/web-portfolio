d37216e06f5cbd9b33556d0e3555a23054b6ac26
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-28 09:54:48 -0600

    Update skills - break into top and all skills

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,7 +4,41 @@ import ExperienceCard from "./ExperienceCard"
 
 export default function Experience() {
 
-  const allSkills = technicalSkills.map((technicalSkill) => (
+  const topSkills = technicalSkills.filter(skill => (skill.topSkill === true))
+  .sort((a, b) => {
+    const skillA = a.skill.toLowerCase();
+    const skillB = b.skill.toLowerCase();
+    if (skillA < skillB) {
+        return -1;
+    }
+    if (skillA > skillB) {
+        return 1;
+    }
+    return 0;
+  });
+
+  const allSkills = technicalSkills.filter(skill => (skill.topSkill === false))
+  .sort((a, b) => {
+    const skillA = a.skill.toLowerCase();
+    const skillB = b.skill.toLowerCase();
+    if (skillA < skillB) {
+        return -1;
+    }
+    if (skillA > skillB) {
+        return 1;
+    }
+    return 0;
+  });
+
+  
+  const sortedTopSkills = topSkills.map((technicalSkill) => (
+    <ExperienceCard
+      key={technicalSkill.id} 
+      technicalSkill={technicalSkill}
+    />
+  ))
+
+  const sortedAllSkills = allSkills.map((technicalSkill) => (
     <ExperienceCard
       key={technicalSkill.id} 
       technicalSkill={technicalSkill}
@@ -12,15 +46,26 @@ export default function Experience() {
   ))
 
   return (
+    <>
     <section id="experience">
     <h5>Technical Skills</h5>
-    <h2>Skills</h2>
+    <h2>Top Languages & Frameworks</h2>
     
     <div className="container experience__container">
       <div className="experience__skills">
-      {allSkills}
+      {sortedTopSkills}
+      </div>
+    </div>
+    
+    </section>
+    <section id="experience">
+        <h2>All Skills & Tools</h2>
+    <div className="container experience__container">
+      <div className="experience__skills">
+      {sortedAllSkills}
       </div>
     </div>
     </section>
+    </>
   )
 }
--- src/components/experience/skills.jsx
+++ src/components/experience/skills.jsx
@@ -2,7 +2,8 @@ import {AiOutlineConsoleSql, AiFillHtml5} from "react-icons/ai"
 import {DiJavascript1, DiPython, DiCss3, DiSass, DiReact, DiPostgresql} from "react-icons/di"
 import {TbFlask, TbBrandOauth} from "react-icons/tb"
 import {SiSqlite, SiMongodb, SiExpress, SiMongoose, 
-    SiSemanticuireact, SiNodedotjs, SiNextdotjs, SiTailwindcss, SiRender} from "react-icons/si"
+    SiSemanticuireact, SiNodedotjs, SiNextdotjs, SiTailwindcss, SiRender, 
+    SiCsharp, SiDotnet, SiLinux, SiKubernetes, SiJenkins, SiAnsible, SiTerraform} from "react-icons/si"
 import {FiTarget} from "react-icons/fi"
 import {PiFileSqlFill} from 'react-icons/pi'
 import {BsGit, BsFillBootstrapFill} from 'react-icons/bs'
@@ -13,127 +14,194 @@ const technicalSkills = [
     {
         id: 1,
         icon: <AiFillHtml5 />,
-        skill: "HTML 5"
+        skill: "HTML 5",
+        topSkill: true
     },
     {
         id: 2,
         icon: <DiJavascript1 />,
-        skill: "JavaScript"
+        skill: "JavaScript",
+        topSkill: true
     },
     {
         id: 3,
         icon: <DiPython />,
-        skill: "Python"
+        skill: "Python",
+        topSkill: true
     },
     {
         id: 4,
         icon: <AiOutlineConsoleSql />,
-        skill: "SQL"
+        skill: "SQL",
+        topSkill: true
     },
     {
         id: 5,
         icon: <DiCss3 />,
-        skill: "CSS 3"
+        skill: "CSS 3",
+        topSkill: true
     },
     {
         id: 6,
         icon: <DiSass />,
-        skill: "Sass"
+        skill: "Sass",
+        topSkill: false
     },
     {
         id: 7,
         icon: <DiReact />,
-        skill: "React.js"
+        skill: "React.js",
+        topSkill: true
     },
     {
         id: 8,
         icon: <DiPostgresql />,
-        skill: "PostgreSQL"
+        skill: "PostgreSQL",
+        topSkill: true
     },
     {
         id: 9,
         icon: <TbFlask />,
-        skill: "Flask"
+        skill: "Flask",
+        topSkill: true
     },
     {
         id: 10,
         icon: <SiRender />,
-        skill: "Render"
+        skill: "Render",
+        topSkill: false
     },
     {
         id: 11,
         icon: <SiSemanticuireact />,
-        skill: "Semantic UI"
+        skill: "Semantic UI",
+        topSkill: false
     },
     {
         id: 12,
         icon: <SiSqlite />,
-        skill: "SQLite"
+        skill: "SQLite",
+        topSkill: false
     },
     {
         id: 13,
         icon: <SiTailwindcss />,
-        skill: "Tailwind"
+        skill: "Tailwind",
+        topSkill: true
     },
     {
         id: 14,
         icon: <BsGit />,
-        skill: "Git"
+        skill: "Git",
+        topSkill: false
     },
     {
         id: 15,
         icon: <PiFileSqlFill />,
-        skill: "Alembic"
+        skill: "Alembic",
+        topSkill: false
     },
     {
         id: 16,
         icon: <FiTarget />,
-        skill: "SQLAlchemy"
+        skill: "SQLAlchemy",
+        topSkill: true
     },
     {
         id: 17,
         icon: <BiMobile />,
-        skill: "React Native"
+        skill: "React Native",
+        topSkill: false
     },
     {
         id: 18,
         icon: <SiNextdotjs />,
-        skill: "Next.js"
+        skill: "Next.js",
+        topSkill: false
     },
     {
         id: 19,
         icon: <SiNodedotjs />,
-        skill: "Node.js"
+        skill: "Node.js",
+        topSkill: false
     },
     {
         id: 20,
         icon: <BsFillBootstrapFill />,
-        skill: "Bootstrap"
+        skill: "Bootstrap",
+        topSkill: false
     },
     {
         id: 21,
         icon: <SiExpress />,
-        skill: "Express"
+        skill: "Express",
+        topSkill: false
     },
     {
         id: 23,
         icon: <SiMongodb />,
-        skill: "MongoDB"
+        skill: "MongoDB",
+        topSkill: false
     },
     {
         id: 24,
         icon: <SiMongoose />,
-        skill: "Mongoose"
+        skill: "Mongoose",
+        topSkill: false
     },
     {
         id: 25,
         icon: <TbBrandOauth />,
-        skill: "OAuth"
+        skill: "OAuth",
+        topSkill: false
     },
     {
         id: 27,
         icon: <FaDocker />,
-        skill: "Docker"
+        skill: "Docker",
+        topSkill: false
+    },
+    {
+        id: 28,
+        icon: <SiCsharp />,
+        skill: "C#",
+        topSkill: false
+    },
+    {
+        id: 29,
+        icon: <SiDotnet />,
+        skill: ".NET",
+        topSkill: false
+    },
+    {
+        id: 30,
+        icon: <SiLinux />,
+        skill: "Linux",
+        topSkill: false
+    },
+    {
+        id: 31,
+        icon: <SiKubernetes />,
+        skill: "Kubernetes",
+        topSkill: false
+    },
+    {
+        id: 32,
+        icon: <SiJenkins />,
+        skill: "Jenkins",
+        topSkill: false
+    },
+    {
+        id: 33,
+        icon: <SiAnsible />,
+        skill: "Ansible",
+        topSkill: false
+    },
+    {
+        id: 34,
+        icon: <SiTerraform />,
+        skill: "Terraform",
+        topSkill: false
     }
 ]
 
