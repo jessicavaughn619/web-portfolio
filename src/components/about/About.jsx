f50f5f80c9e82372c3df460ab91160ec66ceeeb3
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-25 10:25:59 -0600

    Update about content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,9 +13,10 @@ export default function About() {
           <img src={ME} alt="Jessica Vaughn"/>
         </div>
         <div className="about_text_button">
-        <p>I am a Full Stack Software Engineer transitioning to engineering from education. I worked in education for eight years, teaching music and honing my problem solving and leadership skills.</p>
-        <p>My love for learning, interest in understanding how things work, and desire to positively impact my community initiated my transition to software engineering.</p>
-        <p>I'm looking for a new opportunity in front end, back end, or full stack software development and am open to hybrid, in-person, or remote roles.</p>
+        <p>I am a Full Stack Software Engineer with an extensive background in education and web development.</p>
+        <p>I directed high school band programs for eight years, teaching music and honing my problem solving and leadership skills. 
+        My love for learning, interest in understanding how things work, and desire to positively impact my community initiated my transition to software engineering.</p>
+        <p>I have been designing and implementing full stack web solutions for clients for six months. I am looking for a new opportunity as a full stack developer in a large commercial company.</p>
         <p>I live in Denver with my husband Steve and two dogs and a cat - Molly, Olivia, and Bruce. I enjoy hiking and camping, reading, and exploring the many breweries in and around our city.</p>
         <div className="btn-wrapper">
         <a href="#contact" className="btn btn-primary">Let's Connect</a>
