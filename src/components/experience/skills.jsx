573733fac17c7db0c9dd9d730e99231c44418b22
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-11-27 09:37:02 -0700

    Update skills, update resume

--- src/components/experience/skills.jsx
+++ src/components/experience/skills.jsx
@@ -1,15 +1,14 @@
 import {AiOutlineConsoleSql, AiFillHtml5} from "react-icons/ai"
-import {DiJavascript1, DiPython, DiCss3, DiSass, DiReact, DiPostgresql, DiRuby} from "react-icons/di"
+import {DiPython, DiCss3, DiSass, DiReact, DiPostgresql, DiRuby} from "react-icons/di"
 import {TbFlask, TbBrandOauth} from "react-icons/tb"
-import {SiSqlite, SiMongodb, SiExpress, SiMongoose, 
+import {SiSqlite, SiMongodb, SiExpress, 
     SiSemanticuireact, SiNodedotjs, SiNextdotjs, SiTailwindcss, SiRender, 
-    SiCsharp, SiDotnet, SiLinux, SiKubernetes, SiJenkins, SiAnsible, SiTerraform,
-    SiRubyonrails} from "react-icons/si"
+    SiRubyonrails, SiTypescript, SiJavascript} from "react-icons/si"
 import {FiTarget} from "react-icons/fi"
 import {PiFileSqlFill} from 'react-icons/pi'
 import {BsGit, BsFillBootstrapFill} from 'react-icons/bs'
-import {BiMobile, BiLogoDrupal, BiLogoWordpress} from 'react-icons/bi'
-import {FaDocker} from 'react-icons/fa'
+import {BiMobile, BiLogoDrupal} from 'react-icons/bi'
+import {FaDocker, FaGithub} from 'react-icons/fa'
 
 const technicalSkills = [
     {
@@ -22,7 +21,7 @@ const technicalSkills = [
     },
     {
         id: 2,
-        icon: <DiJavascript1 />,
+        icon: <SiJavascript />,
         skill: "JavaScript",
         topSkill: true,
         category: "frontEnd",
@@ -34,7 +33,7 @@ const technicalSkills = [
         skill: "Python",
         topSkill: true,
         category: "backEnd",
-        order: 3
+        order: 5
     },
     {
         id: 4,
@@ -42,34 +41,34 @@ const technicalSkills = [
         skill: "SQL",
         topSkill: true,
         category: "backEnd",
-        order: 8
+        order: 13
     },
     {
-        id: 35,
+        id: 5,
         icon: <DiRuby />,
         skill: "Ruby",
         topSkill: true,
         category: "backEnd",
-        order: 5
+        order: 7
     },
     {
-        id: 36,
+        id: 6,
         icon: <SiRubyonrails />,
         skill: "Rails",
         topSkill: true,
         category: "backEnd",
-        order: 6
+        order: 8
     },
     {
-        id: 37,
+        id: 7,
         icon: <BiLogoDrupal />,
         skill: "Drupal",
         topSkill: true,
         category: "cms",
-        order: 7
+        order: 15
     },
     {
-        id: 5,
+        id: 8,
         icon: <DiCss3 />,
         skill: "CSS",
         topSkill: true,
@@ -77,144 +76,140 @@ const technicalSkills = [
         order: 10
     },
     {
-        id: 7,
+        id: 9,
         icon: <DiReact />,
         skill: "React.js",
         topSkill: true,
         category: "frontEnd",
-        order: 2
+        order: 3
     },
     {
-        id: 14,
+        id: 10,
         icon: <BsGit />,
         skill: "Git",
         topSkill: true,
         category: "devOps",
-        order: 12
+        order: 17
     },
     {
-        id: 9,
+        id: 11,
         icon: <TbFlask />,
         skill: "Flask",
         topSkill: true,
         category: "backEnd",
-        order: 4
+        order: 6
+    },
+    {
+        id: 12,
+        icon: <SiTailwindcss />,
+        skill: "Tailwind",
+        topSkill: true,
+        category: "frontEnd",
+        order: 12,
     },
     {
         id: 13,
-        icon: <SiTailwindcss />,
-        skill: "Tailwind",
-        topSkill: false,
-        category: "frontEnd",
-    },
-    {
-        id: 27,
         icon: <FaDocker />,
         skill: "Docker",
         topSkill: true,
         category: "devOps",
-        order: 11
+        order: 16
     },
     {
-        id: 8,
+        id: 14,
         icon: <DiPostgresql />,
         skill: "PostgreSQL",
         topSkill: false,
         category: "backEnd"
     },
     {
-        id: 16,
+        id: 15,
         icon: <FiTarget />,
         skill: "SQLAlchemy",
         topSkill: false,
         category: "backEnd"
     },
     {
-        id: 10,
+        id: 16,
         icon: <SiRender />,
         skill: "Render",
         topSkill: false,
         category: "devOps"
     },
     {
-        id: 11,
+        id: 17,
         icon: <SiSemanticuireact />,
         skill: "Semantic UI",
         topSkill: false,
         category: "frontEnd"
     },
     {
-        id: 12,
+        id: 18,
         icon: <SiSqlite />,
         skill: "SQLite",
         topSkill: false,
         category: "backEnd"
     },
     {
-        id: 6,
+        id: 19,
         icon: <DiSass />,
         skill: "Sass",
-        topSkill: false,
-        category: "frontEnd"
+        topSkill: true,
+        category: "frontEnd",
+        order: 11
     },
     {
-        id: 15,
+        id: 20,
         icon: <PiFileSqlFill />,
         skill: "Alembic",
         topSkill: false,
         category: "backEnd"
     },
     {
-        id: 17,
+        id: 21,
         icon: <BiMobile />,
         skill: "React Native",
         topSkill: false,
         category: "frontEnd"
     },
     {
-        id: 18,
+        id: 22,
         icon: <SiNextdotjs />,
         skill: "Next.js",
         topSkill: false,
         category: "frontEnd"
     },
     {
-        id: 19,
+        id: 23,
         icon: <SiNodedotjs />,
         skill: "Node.js",
-        topSkill: false,
-        category: "backEnd"
+        topSkill: true,
+        category: "backEnd",
+        order: 4
     },
     {
-        id: 20,
+        id: 24,
         icon: <BsFillBootstrapFill />,
         skill: "Bootstrap",
         topSkill: false,
         category: "frontEnd"
     },
     {
-        id: 21,
+        id: 25,
         icon: <SiExpress />,
         skill: "Express",
         topSkill: false,
         category: "backEnd"
     },
     {
-        id: 23,
+        id: 26,
         icon: <SiMongodb />,
         skill: "MongoDB",
         topSkill: false,
         category: "backEnd"
     },
     {
-        id: 24,
-        icon: <SiMongoose />,
-        skill: "Mongoose",
-        topSkill: false,
-        category: "backEnd"
-    },
-    {
-        id: 25,
+        id: 27,
         icon: <TbBrandOauth />,
         skill: "OAuth",
         topSkill: false,
@@ -222,59 +217,19 @@ const technicalSkills = [
     },
     {
         id: 28,
-        icon: <SiCsharp />,
-        skill: "C#",
-        topSkill: false,
-        category: "backEnd"
+        icon: <SiTypescript />,
+        skill: "TypeScript",
+        topSkill: true,
+        category: "frontEnd",
+        order: 2
     },
     {
         id: 29,
-        icon: <SiDotnet />,
-        skill: ".NET",
-        topSkill: false,
-        category: "backEnd"
-    },
-    {
-        id: 30,
-        icon: <SiLinux />,
-        skill: "Linux",
-        topSkill: false,
-        category: "devOps"
-    },
-    {
-        id: 31,
-        icon: <SiKubernetes />,
-        skill: "Kubernetes",
-        topSkill: false,
-        category: "devOps"
-    },
-    {
-        id: 32,
-        icon: <SiJenkins />,
-        skill: "Jenkins",
-        topSkill: false,
-        category: "devOps"
-    },
-    {
-        id: 33,
-        icon: <SiAnsible />,
-        skill: "Ansible",
-        topSkill: false,
-        category: "devOps"
-    },
-    {
-        id: 34,
-        icon: <SiTerraform />,
-        skill: "Terraform",
-        topSkill: false,
-        category: "devOps"
-    },
-    {
-        id: 38,
-        icon: <BiLogoWordpress />,
-        skill: "WordPress",
-        topSkill: false,
-        category: "cms"
+        icon: <FaGithub />,
+        skill: "GitHub",
+        topSkill: true,
+        category: "devOps",
+        order: 18
     }
 ]
 
