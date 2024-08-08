84d8c9cc938e6d80b13e1b48b60d748886c2594e
Jessica Vaughn <jessicavaughn@HIKE2.localdomain>
2024-08-08 09:15:11 -0600

    Update about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,10 +13,8 @@ export default function About() {
           <img src={ME} alt="Jessica Vaughn"/>
         </div>
         <div className="about_text_button">
-        <p>I am a Certified Salesforce Developer with Revolent Group, where I build custom Salesforce solutions for clients. Prior to joining Revolent Group, I worked as a full-stack web developer
-          designing and building solutions for small businesses and non-profit organizations.</p>
-        <p>My professional career began in the Education industry, where I taught high school band for 8 years. Teaching honed my identity as a lifelong learner, taught me how to collaborate effectively with a dynamic team, to analyze and make data-driven decisions, 
-          and strengthened my resilience and problem solving skills in the face of challenges. Each of these skills transferred beautifully into my career as a Software Engineer.</p>
+        <p>I am a 10x Certified Salesforce Consultant & Developer with Hike2, where I design and build custom cloud-solutions for clients. Prior to joining Hike2, I worked as a full-stack web developer designing and building solutions for small businesses and non-profit organizations.</p>
+        <p>My professional career began in the Education industry, where I taught high school band for 8 years. I am a lifelong learner adept in mastering new skills and technologies, a dynamic team leader and contributor, and a problem-solver and solution-builder at my core.</p>
         <p>I live in Denver with my husband Steve and two dogs and a cat - Molly, Olivia, and Bruce. I enjoy hiking and camping, listening to and performing music, reading, and exploring the many breweries in and around our city.</p>
         <div className="btn-wrapper">
         <a href="#contact" className="btn btn-primary">Let's Connect</a>
