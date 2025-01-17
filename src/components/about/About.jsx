a9c57dca60a1e9d74dc4e2f11077c38a9ddc5ff6
Jessica Vaughn <jessicavaughn@Mac.localdomain>
2025-01-17 11:10:13 -0700

    Update certification count

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
           <img src={ME} alt="Jessica Vaughn"/>
         </div>
         <div className="about_text_button">
-        <p>I am a 10x Certified Salesforce Consultant & Developer with Hike2, where I design and build custom cloud-solutions for clients. Prior to joining Hike2, I worked as a full-stack web developer designing and building solutions for small businesses and non-profit organizations.</p>
+        <p>I am a 14x Certified Salesforce Developer & Consultant with Hike2, where I design and build custom cloud-solutions for clients. Prior to joining Hike2, I worked as a full-stack web developer designing and building solutions for small businesses and non-profit organizations.</p>
         <p>My professional career began in the Education industry, where I taught high school band for 8 years. I am a lifelong learner adept in mastering new skills and technologies, a dynamic team leader and contributor, and a problem-solver and solution-builder at my core.</p>
         <p>I live in Denver with my husband Steve and two dogs and a cat - Molly, Olivia, and Bruce. I enjoy hiking and camping, listening to and performing music, reading, and exploring the many breweries in and around our city.</p>
         <div className="btn-wrapper">
